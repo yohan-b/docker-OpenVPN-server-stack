e850b739027aa60d29a1cceb58c011f450ddc435
yohan <783b8c87@scimetis.net>
2019-12-31 20:46:17 +0100

    Generalization.

--- create_conf.sh
+++ create_conf.sh
@@ -4,18 +4,19 @@ cd conf
 openssl req -nodes -days 3650 -new -x509 -keyout ca.key -out ca.crt -subj "/C=FR/O=scimetis/CN=scimetis.net" 
 openssl dhparam -out dh1024.pem 1024
 
-openssl req -nodes -new -keyout server.key -out server.csr -subj "/C=FR/O=scimetis/CN=scimetis.net" -reqexts server -config ../openssl.conf
+openssl req -nodes -new -keyout server.key -out server.csr -subj "/C=FR/O=scimetis/CN=ovh1.scimetis.net" -reqexts server -config ../openssl.conf
 openssl x509 -req -days 3650 -CA ca.crt -CAkey ca.key -CAcreateserial -extensions server -extfile ../openssl.conf -in server.csr -out server.crt
 
-openssl req -nodes -new -keyout client.key -out client.csr -subj "/C=FR/O=scimetis/CN=serveur Windows" -reqexts usr_cert -config ../openssl.conf
+openssl req -nodes -new -keyout client.key -out client.csr -subj "/C=FR/O=scimetis/CN=serveur.scimetis.net" -reqexts usr_cert -config ../openssl.conf
 openssl x509 -req -days 3650 -CA ca.crt -CAkey ca.key -CAcreateserial -extensions usr_cert -extfile ../openssl.conf -in client.csr -out client.crt
 
-openssl req -nodes -new -keyout client2.key -out client2.csr -subj "/C=FR/O=scimetis/CN=PC client" -reqexts usr_cert -config ../openssl.conf
+openssl req -nodes -new -keyout client2.key -out client2.csr -subj "/C=FR/O=scimetis/CN=serveur-appart.scimetis.net" -reqexts usr_cert -config ../openssl.conf
 openssl x509 -req -days 3650 -CA ca.crt -CAkey ca.key -CAcreateserial -extensions usr_cert -extfile ../openssl.conf -in client2.csr -out client2.crt
 
 chcon -R -u system_u -r object_r -t svirt_sandbox_file_t ./
 mkdir keys
 mkdir ccd
 cp -a ca.crt ca.srl dh1024.pem server.crt server.key keys/
-echo "ifconfig-push 192.168.102.10 255.255.255.0" > "ccd/serveur Windows"
+echo "ifconfig-push 192.168.102.10 255.255.255.0" > "ccd/serveur.scimetis.net"
+echo "ifconfig-push 192.168.102.3 255.255.255.0" > "ccd/serveur-appart.scimetis.net"
 cd ..
---
+++ start_or_update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source vars
+test -z ${KEY} && { echo "KEY variable is not defined."; exit 1; }
+test -z $1 || HOST="_$1"
+test -z $2 || INSTANCE="_$2"
+
+sudo rm -f conf/keys/* conf/ccd/*
+
+test -f ~/secrets.tar.gz.enc || curl -o ~/secrets.tar.gz.enc "https://${CLOUD_SERVER}/s/${KEY}/download?path=%2F&files=secrets.tar.gz.enc"
+openssl enc -aes-256-cbc -d -in ~/secrets.tar.gz.enc | sudo tar -zxv --strip 2 secrets/docker-OpenVPN-server-stack${HOST}${INSTANCE}/conf/keys
+
+mkdir -p conf/ccd
+rm -rf ~/config
+git clone https://${GIT_SERVER}/yohan/config.git ~/config
+sudo cp -a ~/config/docker-OpenVPN-server-stack${HOST}${INSTANCE}/server.conf ./
+sudo cp -a ~/config/docker-OpenVPN-server-stack${HOST}${INSTANCE}/ccd/* conf/ccd/
+rm -rf ~/config
+sudo chown -R root. conf server.conf
+
+# --force-recreate is used to recreate container when crontab file has changed
+unset VERSION_OPENVPN_SERVER
+VERSION_OPENVPN_SERVER=$(git ls-remote https://${GIT_SERVER}/yohan/docker-OpenVPN-server.git| head -1 | cut -f 1|cut -c -10) \
+ sudo -E bash -c 'docker-compose up -d --force-recreate'
+
